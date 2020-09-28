cb3bcb85530f4d6ab9486fec198f7345626d8180
bbudano <bbudano@gmail.com>
2020-09-28 16:56:06 +0200

    Initial commit

---
+++ src/main/java/hr/bernardbudano/slamalandart/SlamalandartApplication.java
@@ -0,0 +1,13 @@
+package hr.bernardbudano.slamalandart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlamalandartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlamalandartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/bernardbudano/slamalandart/SlamalandartApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.bernardbudano.slamalandart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlamalandartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
